7105da30824ae827d7ab5c9db876813fc5f20fad
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-10 17:24:32 +0300

    Update PartSize.cs

--- Player/PartSize.cs
+++ Player/PartSize.cs
@@ -4,7 +4,5 @@ using UnityEngine;
 public class PartSize : MonoBehaviour
 {
     public IntReactiveProperty size { get; private set; } = new IntReactiveProperty(1);
-
-    // Äðóãèå ìåòîäû è ëîãèêà äëÿ èçìåíåíèÿ ðàçìåðà ÷àñòè òåëà
     
 }
