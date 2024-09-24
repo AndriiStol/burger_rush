137b41d342a1c0dd2c453fd6e8ba4a617e2fe5c5
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-24 17:27:57 +0300

    Update ScoreDisplay.cs

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -4,19 +4,19 @@ using UnityEngine.UI;
 public class ScoreDisplay : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
-    [SerializeField] private float textAppearDelay = 0.1f; // Задержка перед появлением каждой цифры
+    [SerializeField] private float textAppearDelay = 0.1f; 
     private int targetScore;
     private int currentScore;
 
     private void Start ()
     {
-        // Устанавливаем начальное значение текста
+        
         scoreText.text = "Your Score: 0";
         currentScore = 0;
         targetScore = 0;
     }
 
-    // Метод для обновления отображаемого количества бургеров
+    
     public void UpdateScoreDisplay(int score)
     {
         targetScore = score;
@@ -25,17 +25,17 @@ public class ScoreDisplay : MonoBehaviour
 
     private System.Collections.IEnumerator AnimateScore()
     {
-        // Проходим по каждой цифре
+       
         for (int i = currentScore; i <= targetScore; i++)
         {
-            // Задаем текст с учетом текущей цифры
+            
             scoreText.text = "Your Score: " + i.ToString();
 
-            // Ждем заданное время перед следующей цифрой
+            
             yield return new WaitForSeconds(textAppearDelay);
         }
 
-        // Обновляем текущий счет до целевого счета
+       
         currentScore = targetScore;
     }
 }
